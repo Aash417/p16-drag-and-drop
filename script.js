b93a458b46245c601a0d0e3e226bb04e992a3fbd
aashish_k <aashishkathat00@gmail.com>
2023-09-27 01:18:19 +0530

    drag and drop implemented

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
 // Item Lists
-const itemLists = document.querySelectorAll(".drag-item-list");
+const listColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -20,6 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -68,6 +70,8 @@ function createItemEl(columnEl, column, item, index) {
 	const listEl = document.createElement("li");
 	listEl.classList.add("drag-item");
 	listEl.textContent = item;
+	listEl.draggable = true;
+	listEl.setAttribute("ondragstart", "drag(event)");
 
 	// append
 	columnEl.appendChild(listEl);
@@ -103,6 +107,36 @@ function updateDOM() {
 	});
 
 	// Run getSavedColumns only once, Update Local Storage
+}
+
+// When item start drag
+function drag(e) {
+	draggedItem = e.target;
+	// console.log(draggedItem);
+}
+
+// column allows for items to drop
+function allowDrop(e) {
+	e.preventDefault();
+}
+
+// when item enter column area
+function dragEnter(col) {
+	// console.log(listColumns[col]);
+	listColumns[col].classList.add("over");
+	currentColumn = col;
+}
+
+// droping item in column
+function drop(e) {
+	e.preventDefault();
+	// remove background color/padding
+	listColumns.forEach((col) => {
+		col.classList.remove("over");
+	});
+	// add item to column
+	const parent = listColumns[currentColumn];
+	parent.appendChild(draggedItem);
 }
 
 // on load
