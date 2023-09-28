e5bd1c4d2bb328159c4b3c4104e2ce149c27ded9
aashish_k <aashishkathat00@gmail.com>
2023-09-28 14:50:34 +0530

    refactor with map

--- script.js
+++ script.js
@@ -165,23 +165,36 @@ function hideInputBox(col) {
 
 // allow array to update on ls
 function rebuildArrays() {
-	backlogListArray = [];
-	for (let i = 0; i < backlogList.children.length; i++) {
-		backlogListArray.push(backlogList.children[i].textContent);
-	}
-	progressListArray = [];
-	for (let i = 0; i < progressList.children.length; i++) {
-		progressListArray.push(progressList.children[i].textContent);
-	}
-	completeListArray = [];
-	for (let i = 0; i < completeList.children.length; i++) {
-		completeListArray.push(completeList.children[i].textContent);
-	}
-	onHoldListArray = [];
-	for (let i = 0; i < onHoldList.children.length; i++) {
-		onHoldListArray.push(onHoldList.children[i].textContent);
-	}
+	backlogListArray = Array.from(backlogList.children).map((e) => e.textContent);
+
+	progressListArray = Array.from(progressList.children).map(
+		(e) => e.textContent
+	);
+
+	completeListArray = Array.from(completeList.children).map(
+		(e) => e.textContent
+	);
+
+	onHoldListArray = Array.from(onHoldList.children).map((e) => e.textContent);
+
 	updateDOM();
+	// backlogListArray = [];
+	// for (let i = 0; i < backlogList.children.length; i++) {
+	// 	backlogListArray.push(backlogList.children[i].textContent);
+	// }
+	// progressListArray = [];
+	// for (let i = 0; i < progressList.children.length; i++) {
+	// 	progressListArray.push(progressList.children[i].textContent);
+	// }
+	// completeListArray = [];
+	// for (let i = 0; i < completeList.children.length; i++) {
+	// 	completeListArray.push(completeList.children[i].textContent);
+	// }
+	// onHoldListArray = [];
+	// for (let i = 0; i < onHoldList.children.length; i++) {
+	// 	onHoldListArray.push(onHoldList.children[i].textContent);
+	// }
+	// updateDOM();
 }
 
 // When item start drag
